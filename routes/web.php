8c02e718fbe22f74f6665db888fef07865ca5177
Sofia Calderon <sofia.calderon@masivian.com>
2021-04-29 08:24:50 -0500

    Inicio de PHP Basic, con pruebas basica de Arrays - Basado en Laravel

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Callback: Función sin nombre, que hace parte de los parametrod de la Invocación de otra.
+Route::get('/', function () {
+    return view('welcome');
+});
+//JAVA, .NET: Fuertemente Tipados.
+//PHP, PYTHON, JAVASCRIPT: Debilmente Tipado.
+Route::get('variables', function () {
+    $mensaje = 8;
+    var_dump($mensaje);
+    echo "<hr />";
+    $mensaje = "Sofía Calderón! Nice!";
+    var_dump($mensaje);
+    echo "<br />";
+    $mensaje = true;
+    var_dump($mensaje);
+});
+Route::get('dates', function () {
+    //Array-> Es una estructura de Datos-Almacenando 1 o más Datos.
+    $estudiantes = [    "Diego",
+                        "Matias",
+                        "Juan" ];
+    echo "<pre>";
+    print_r($estudiantes);
+    echo "<pre>";
+});
